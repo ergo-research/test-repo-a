d17a07b91d0e64cf1a074bd51bd13d43b80bd3fb
Emanuel H. Rubensson <emanuelrubensson@gmail.com>
2013-04-15 21:47:01 +0200

    Added another test program.

---
+++ emanuelprogram.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+  std::cout << "Hej hej!" << std::endl;
+  std::cout << "Hej hej hej." << std::endl;
+  std::cout << "Hej hej hej hej." << std::endl;
+  return 0;
+}
